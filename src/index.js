5b2d0f8dc71b824bec588e0b7af61fcdf736a917
nguyenduchoa99 <hoarowa22df@gmail.com>
2022-12-07 18:44:32 +0700

    upload 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//yarn add express
+const express = require('express');
+const app = express();
+//middleware chuyển đổi data json từ FE xuống express
+app.use(express.json());
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const cors = require('cors');
+app.use(cors());
+//domain
+app.listen(8080)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const mysql = require('mysql2');
+// //kết nối CSDL
+// const conn = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "1234",
+//     database: "db_node",
+//     port: 3306
+// })
+
+const rootRoute = require('./routes');
+
+app.use("/api",rootRoute)
